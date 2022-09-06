363fa817fb9d9fc4a63551bf5146aaf254b453c0
Ezra Chai <juanzhe2@gmail.com>
2022-09-06 22:50:22 +0800

    fixed bugs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  // site: 'https://EzraChai.github.io',
+  site: 'https://ezrachai.github.io',
 });
