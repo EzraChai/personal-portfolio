cd3d023bdfd16f6891daf944c568d31060e7aaf5
Ezra Chai <juanzhe2@gmail.com>
2022-09-06 22:59:13 +0800

    added netlify config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
+import netlify from '@astrojs/netlify/edge-functions';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   site: 'https://ezrachai.github.io',
+  output: "server",
+  adapter: netlify()
 });
