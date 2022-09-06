2025acafdbb51623e2f51eaf30a6a85b8b288765
Ezra Chai <juanzhe2@gmail.com>
2022-09-06 21:52:32 +0800

    fixed bugs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import deno from "@astrojs/deno";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), image()],
   output: "server",
   adapter: deno()
 });
