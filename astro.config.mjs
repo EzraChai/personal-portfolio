5e4ba272fa4be4fd1a1a5e8fc8522b9fc2d1b166
Ezra Chai <juanzhe2@gmail.com>
2022-09-06 21:37:14 +0800

    added deno

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import deno from "@astrojs/deno";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  output: "server",
+  adapter: deno()
 });
