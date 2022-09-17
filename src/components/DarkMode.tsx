a61f5663ec06f3d82deeffee392561440897540a
Ezra Chai <juanzhe2@gmail.com>
2022-09-17 09:30:30 +0800

    prettier

--- src/components/DarkMode.tsx
+++ src/components/DarkMode.tsx
@@ -1,26 +1,37 @@
-import SunSvg from "../images/Sun.svg"
-import MoonSvg from "../images/Moon.svg"
-import { useEffect, useState } from 'react';
-
+import SunSvg from "../images/Sun.svg";
+import MoonSvg from "../images/Moon.svg";
+import { useEffect, useState } from "react";
 
 export default function DarkMode() {
+  const [theme, setTheme] = useState<string>(
+    localStorage.getItem("theme") ?? "light"
+  );
 
-    const [theme, setTheme] = useState<string>(localStorage.getItem("theme") ?? "light")
+  const toggleDarkMode = () => {
+    setTheme((prev) => (prev === "dark" ? "light" : "dark"));
+  };
 
-    const toggleDarkMode = () => {
-        setTheme((prev) => prev === "dark"? "light": "dark")
-    }
-
-    useEffect(() => {
+  useEffect(() => {
     if (theme === "dark") {
-        document.documentElement.classList.add("dark");
+      document.documentElement.classList.add("dark");
     } else {
-        document.documentElement.classList.remove("dark");
+      document.documentElement.classList.remove("dark");
     }
     localStorage.setItem("theme", theme);
-    },[theme])
+  }, [theme]);
 
-    return (
-        <button className={theme &&"rounded-md p-1 border-2 dark:border-zinc-200 border-zinc-600 "} onClick={toggleDarkMode}>{theme==="light"?<img height={"20"} width={"20"} src={SunSvg}/>:<img height={"20"} width={"20"} src={MoonSvg}/>}</button>
-    )
+  return (
+    <button
+      className={
+        theme && "rounded-md p-1 border-2 dark:border-zinc-200 border-zinc-600 "
+      }
+      onClick={toggleDarkMode}
+    >
+      {theme === "light" ? (
+        <img height={"20"} width={"20"} src={SunSvg} />
+      ) : (
+        <img height={"20"} width={"20"} src={MoonSvg} />
+      )}
+    </button>
+  );
 }
