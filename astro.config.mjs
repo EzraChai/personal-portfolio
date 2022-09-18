051214155717a1d85afda75b42dd709c53738d9d
Ezra Chai <juanzhe2@gmail.com>
2022-09-18 14:00:49 +0800

    added astro-compress integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-    site,
-    integrations: [react(), tailwind(), sitemap()],
+  site: "https://ezrachai.pages.dev",
+  integrations: [react(), tailwind(), sitemap(), compress()]
 });
