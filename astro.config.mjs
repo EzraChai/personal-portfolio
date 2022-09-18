bb63ce53576a1e55c09357cc2fdc1f9dec374c90
Ezra Chai <juanzhe2@gmail.com>
2022-09-18 14:07:19 +0800

    added partytown and google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import compress from "astro-compress";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ezrachai.pages.dev",
-  integrations: [react(), tailwind(), sitemap(), compress()]
+  integrations: [react(), tailwind(), sitemap(), compress(), partytown()]
 });
