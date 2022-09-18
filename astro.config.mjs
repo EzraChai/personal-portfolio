a10d625a9043998a12036ec2f979d09a91aa7669
Ezra Chai <juanzhe2@gmail.com>
2022-09-18 13:57:57 +0800

    added sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react(), tailwind()],
+    site,
+    integrations: [react(), tailwind(), sitemap()],
 });
