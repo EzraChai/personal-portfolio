7d3a30946c647fcffdac701a98612b7e9fec3e81
Ezra Chai <juanzhe2@gmail.com>
2022-09-18 14:19:32 +0800

    removed partytown and google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,8 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import compress from "astro-compress";
 
-import partytown from "@astrojs/partytown";
-
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ezrachai.pages.dev",
-  integrations: [react(), tailwind(), sitemap(), compress(), partytown()]
+    site: "https://ezrachai.pages.dev",
+    integrations: [react(), tailwind(), sitemap(), compress()],
 });
