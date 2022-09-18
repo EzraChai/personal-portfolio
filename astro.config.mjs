d7d8441c5ceeb226ae71326b59df716b1c79e409
Ezra Chai <juanzhe2@gmail.com>
2022-09-18 14:23:25 +0800

    added robotsTxt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import compress from "astro-compress";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://ezrachai.pages.dev",
-    integrations: [react(), tailwind(), sitemap(), compress()],
+    experimental: {
+        integrations: true,
+    },
+    integrations: [react(), tailwind(), sitemap(), compress(), robotsTxt()],
 });
